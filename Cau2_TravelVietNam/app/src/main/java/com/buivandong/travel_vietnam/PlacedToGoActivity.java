cee7ef4849c4fe97aaa8e4c7f7f3005ff1fe5ba0
Van Dong <vandong78py@gmail.com>
2024-03-24 12:37:48 +0700

    Travel VietNam

---
+++ Cau2_TravelVietNam/app/src/main/java/com/buivandong/travel_vietnam/PlacedToGoActivity.java
@@ -0,0 +1,14 @@
+package com.buivandong.travel_vietnam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlacedToGoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_placed_to_go);
+    }
+}
