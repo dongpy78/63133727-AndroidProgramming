99d9622cb8c1f239bd14f199ba580fb4228d25da
Van Dong <vandong78py@gmail.com>
2024-03-24 13:26:20 +0700

    update code

---
+++ Cau2_TravelVietNam/app/src/main/java/com/buivandong/travel_vietnam/ItemTravelListAdapter.java
@@ -0,0 +1,11 @@
+package com.buivandong.travel_vietnam;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ItemTravelListAdapter {
+
+    static class ItemTravelViewHolder {
+
+    }
+
+}
--- Cau2_TravelVietNam/app/src/main/java/com/buivandong/travel_vietnam/TravelVietNamActivity.java
+++ Cau2_TravelVietNam/app/src/main/java/com/buivandong/travel_vietnam/TravelVietNamActivity.java
@@ -10,5 +10,19 @@ public class TravelVietNamActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_travel_viet_nam);
+        setUpList();
+    }
+
+    private void setUpList() {
+        ItemTravelList[] travelVietNam = {
+                new ItemTravelList("Phú yên", "Phú Yên đẹp lắm", R.drawable.canh_dep_ninh_binh_viet_nam_055419852, 5),
+                new ItemTravelList("Phú yên", "Phú Yên đẹp lắm", R.drawable.canh_dep_ninh_binh_viet_nam_055419852, 5),
+                new ItemTravelList("Phú yên", "Phú Yên đẹp lắm", R.drawable.canh_dep_ninh_binh_viet_nam_055419852, 5),
+                new ItemTravelList("Phú yên", "Phú Yên đẹp lắm", R.drawable.canh_dep_ninh_binh_viet_nam_055419852, 5),
+                new ItemTravelList("Phú yên", "Phú Yên đẹp lắm", R.drawable.canh_dep_ninh_binh_viet_nam_055419852, 5),
+                new ItemTravelList("Phú yên", "Phú Yên đẹp lắm", R.drawable.canh_dep_ninh_binh_viet_nam_055419852, 5),
+                new ItemTravelList("Phú yên", "Phú Yên đẹp lắm", R.drawable.canh_dep_ninh_binh_viet_nam_055419852, 5),
+
+        };
     }
 }
