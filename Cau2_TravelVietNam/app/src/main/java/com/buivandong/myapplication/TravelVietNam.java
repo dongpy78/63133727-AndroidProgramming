b71ca8991430f0fd668e2715a13ee8bba29a8ba2
Van Dong <vandong78py@gmail.com>
2024-03-25 10:17:45 +0700

    cap nhat du lieu list view

--- Cau2_TravelVietNam/app/src/main/java/com/buivandong/myapplication/TravelVietNam.java
+++ Cau2_TravelVietNam/app/src/main/java/com/buivandong/myapplication/TravelVietNam.java
@@ -22,12 +22,12 @@ public class TravelVietNam extends AppCompatActivity {
         ListView lvTravelVN = (ListView) findViewById(R.id.list_view_travel);
         // Dữ liệu
         ArrayList<TravelListData> dsTravel = new ArrayList<TravelListData>();
-        dsTravel.add(new TravelListData("Phú Yên", "Phú Yên Quê tôi!", R.drawable.canh_dep_ha_long_viet_nam_055419665, 4.5f));
-        dsTravel.add(new TravelListData("Phú Yên", "Phú Yên Quê tôi!", R.drawable.canh_dep_ha_long_viet_nam_055419665, 4.5f));
-        dsTravel.add(new TravelListData("Phú Yên", "Phú Yên Quê tôi!", R.drawable.canh_dep_ha_noi_viet_nam_055419712, 4.5f));
-        dsTravel.add(new TravelListData("Phú Yên", "Phú Yên Quê tôi!", R.drawable._jf0b7075nbc1695660163415, 4.5f));
-        dsTravel.add(new TravelListData("Phú Yên", "Phú Yên Quê tôi!", R.drawable.canh_dep_ha_long_viet_nam_055419665, 4.5f));
-        dsTravel.add(new TravelListData("Phú Yên", "Phú Yên Quê tôi!", R.drawable.canh_dep_ninh_binh_viet_nam_055419852, 4.5f));
+        dsTravel.add(new TravelListData("Tháp Trầm Hương", "Tháp Trầm Hương là một công trình kiến trúc biểu tượng của Nha Trang, Khánh Hòa. Tòa tháp nằm tại Quảng trường 2/4 dọc theo đường Trần Phú và gần Trung tâm văn hoá Khánh Hòa.", R.drawable.sdfsdf_12, 4.5f));
+        dsTravel.add(new TravelListData("Vịnh Hạ Long", "Vịnh Hạ Long là quần thể danh lam thắng cảnh của tỉnh Quảng Ninh bao gồm gần 2000 hòn đảo lớn nhỏ. Riêng khu vực được UNESCO công nhận là Di sản thế giới ", R.drawable.canh_dep_ha_long_viet_nam_055419665, 4.5f));
+        dsTravel.add(new TravelListData("Ninh Bình", "Ninh Bình được biết đến là nơi có nguồn tài nguyên du lịch phong phú với nhiều di tích lịch sử, văn hóa, danh lam thắng cảnh nổi tiếng rất có giá trị.", R.drawable.canh_dep_ninh_binh_viet_nam_055419852, 4.5f));
+        dsTravel.add(new TravelListData("Phú Quốc", "Phú Quốc là hòn đảo lớn nhất của Việt Nam. Phú Quốc cùng với các hòn đảo khác tạo thành huyện đảo Phú Quốc thuộc tỉnh Kiên Giang.", R.drawable.phuquoc, 4.5f));
+        dsTravel.add(new TravelListData("Xóm Rớ", "xóm Rớ Phú Yên được biết đến là thiên đường rêu xanh nằm trên địa phận Đông Tác, phường Phú Đông, thành phố Tuy Hòa.", R.drawable.ke_chan_song_xom_ro_2, 4.5f));
+        dsTravel.add(new TravelListData("Phong Nha - Kẻ Bàng", "Nổi tiếng là một trong những kỳ quan thiên nhiên thế giới quy mô nhất tại Việt Nam hiện nay, thuộc địa phận tỉnh Quảng Bình", R.drawable.phong_nha_999, 4.5f));
 
         TravelAdapter adapter = new TravelAdapter(this, dsTravel);
         lvTravelVN.setAdapter(adapter);
